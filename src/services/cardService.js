bc3342819e1460edcf1aa11fe167e2c9b3bfa537
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-22 20:24:51 +0100

    pruebas de creacion

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,18 +1,27 @@
 const API_URL = "http://localhost:4000/cards"
 
-export const getCards = async ()=> {
-    const res = await fetch(API_URL)
-    return res.json()
+
+export const getCards = async () => {
+  const res = await fetch(API_URL)
+  return res.json()
 }
 
-export const createCard = async (cardData) => {
-    const res = await fetch (API_URL, {
-        method:"POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body:JSON.stringify(cardData)
-    })
 
-    return res.json()
+export const createCard = async (cardData) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(cardData)
+  })
+
+  return res.json()
+}
+
+
+export const deleteCard = async (id) => {
+  await fetch(`${API_URL}/${id}`, {
+    method: "DELETE"
+  })
 }
