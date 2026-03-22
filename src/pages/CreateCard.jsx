1c0abf1ce1be35b42cb2636a706e97b91d2cadd7
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-22 20:06:40 +0100

    pruebas de creacion

--- src/pages/CreateCard.jsx
+++ src/pages/CreateCard.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { createCard } from "../services/cardService"
 
 function CreateCard() {
-    const [form,setForm]= useState({
+  const navigate = useNavigate()
+
+  const [form, setForm] = useState({
     characterName: "",
     characterImage: "",
     mediaType: "",
@@ -21,13 +24,11 @@ function CreateCard() {
     e.preventDefault()
 
     try {
-      const newCard = await createCard({
+      await createCard({
         ...form,
         likes: 0,
         likedBy: []
       })
-
-      setCards(prev =>[...prev, newCard])
 
       setForm({
         characterName: "",
@@ -37,27 +38,56 @@ function CreateCard() {
         quote: ""
       })
 
+      navigate("/")
+
     } catch (error) {
       console.error("Error al crear card:", error)
     }
   }
 
   return (
-    <>
     <div>
-        <h2>Crea una nueva card</h2>
+      <h2>Crea una nueva card</h2>
 
-        <form onSubmit={handleSubmit}>
-        <input name="characterName" placeholder="Character" value={form.characterName} onChange={handleChange} />
-        <input name="characterImage" placeholder="Image URL" value={form.characterImage} onChange={handleChange} />
-        <input name="mediaType" placeholder="Type" value={form.mediaType} onChange={handleChange} />
-        <input name="mediaTitle" placeholder="Title" value={form.mediaTitle} onChange={handleChange} />
-        <input name="quote" placeholder="Quote" value={form.quote} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="characterName"
+          placeholder="Character"
+          value={form.characterName}
+          onChange={handleChange}
+        />
+
+        <input
+          name="characterImage"
+          placeholder="Image URL"
+          value={form.characterImage}
+          onChange={handleChange}
+        />
+
+        <input
+          name="mediaType"
+          placeholder="Type"
+          value={form.mediaType}
+          onChange={handleChange}
+        />
+
+        <input
+          name="mediaTitle"
+          placeholder="Title"
+          value={form.mediaTitle}
+          onChange={handleChange}
+        />
+
+        <input
+          name="quote"
+          placeholder="Quote"
+          value={form.quote}
+          onChange={handleChange}
+        />
 
         <button type="submit">Crear Card</button>
       </form>
     </div>
-    </>
   )
 }
 
