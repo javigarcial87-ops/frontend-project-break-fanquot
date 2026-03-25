f22c080ddc4e4ed40d6bd453bbeab5c290a52d96
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-25 19:27:24 +0100

    formulario de registro

---
+++ src/pages/Register.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { loginUser, registerUser } from "../services/authService"
+
+function Register() {
+    const [form, setForm] =useState({
+        username:"",
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const user = await registerUser(form)
+        console.log(user)
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <input name="username" placeholder="Username" onChange={handleChange} />
+            <input name="email" placeholder="Email" onChange={handleChange} />
+            <input name="password" placeholder="Password" onChange={handleChange} />
+            <button>Registrarse</button>
+        </form>
+        </>
+    )
+}
+
+export default Register
