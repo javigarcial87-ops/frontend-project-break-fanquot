e8438d351f3ab027e54932db2b662ef28ef02bef
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-28 21:15:29 +0100

    selector de medio cambio correcciones

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,8 +38,8 @@ return (
         <>
         <form onSubmit={handleSubmit}>
             <h2>LOGIN</h2>
-            <input name="email" placeholder="Email" onChange={handleChange} />
-            <input name="password" placeholder="Password" onChange={handleChange} />
+            <input name="email" placeholder="Email" onChange={handleChange} required />
+            <input name="password" placeholder="Password" onChange={handleChange} required />
             <button>Login</button>
         </form>
         </>
