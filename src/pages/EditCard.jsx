1cd59dea8764fbbfb053c67ba448863b27b54efb
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-28 18:40:26 +0100

    edicion de cards por administrador

---
+++ src/pages/EditCard.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { getCards, updateCard } from "../services/cardService"
+
+function EditCard() {
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    const [form, setForm] = useState({
+        characterName: "",
+        characterImage: "",
+        mediaType: "",
+        mediaTitle: "",
+        quote: ""
+    })
+
+    useEffect(()=> {
+        getCards().then(cards => {
+            const card = cards.find(c => c._id === id)
+            if (card) setForm(card)
+        })
+    },[id])
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const hadleSubmit = async (e) => {
+        e.preventDefault()
+
+        await updateCard(id, form)
+
+        navigate("/admin")
+    }
+
+    return (
+        <>
+    <div>
+            <h2>Editar Card</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input name="characterName" value={form.characterName} onChange={handleChange} />
+                <input name="characterImage" value={form.characterImage} onChange={handleChange} />
+                <input name="mediaType" value={form.mediaType} onChange={handleChange} />
+                <input name="mediaTitle" value={form.mediaTitle} onChange={handleChange} />
+                <input name="quote" value={form.quote} onChange={handleChange} />
+
+                <button type="submit">Guardar</button>
+            </form>
+
+    </div>
+        </>
+    )
+}
+
+export default EditCard
