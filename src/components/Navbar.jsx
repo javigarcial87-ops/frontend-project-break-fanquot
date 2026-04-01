3c83d3d9a2e4ac69c8cdca07a0e96ed4b5e3ee3e
javier garcia alvarez <javigarcial87@gmail.com>
2026-04-01 16:58:32 +0200

    logos resubidos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar() {
   return (
     <>
     <nav className="navBar">
-      <img src="" alt="LOGO" className="imgLogo"/>
-      <Link to="/LOGO-fanquot-bueno.png"><img src="public/iconos_SVG/home.svg" alt="home" className="iconHome" /></Link>
+      <img src="/LOGO-fanquot-bueno.png" alt="LOGO" className="imgLogo"/>
+      <Link to="/"><img src="public/iconos_SVG/home.svg" alt="home" className="iconHome" /></Link>
       <Link to="/create">Crear Card</Link>
       <Link to="/profile">Mi perfil</Link>
 
@@ -23,7 +23,7 @@ function Navbar() {
             localStorage.removeItem("user")
             window.location.reload()
           }} className="btnLogout">
-            <img src="/log-out.svg" alt="logout" className="iconLogout"/>
+            <img src="public/iconos_SVG/log-out.svg" alt="logout" className="iconLogout"/>
           </button>
         </>
       ) : (
